85bd1e8b8f551d543e3cb68a756bbba3a03b56a1
karups-thangaraj <karuppiah.thangaraj@gmail.com>
2020-07-27 00:12:07 +0530

    movies initial

--- database/ddls.php
+++ database/ddls.php
@@ -12,6 +12,26 @@ primary key(mem_id));
 create user 'imovies'@'localhost' identified by 'Vit2020@project';
 grant all privileges on *.* to 'imovies'@'localhost';
 
+//entities with attributes for Ineternet Movies Booking system
+a. Login (Customer / Admin / Owner )   
+    Customer to view movies and shows and book tickets. 
+    Admin to add movies, add shows, update / delete shows
+    Owner to add Screens, view summary reports
+b. Profile   c. Movies   d. Screens   e. Shows   f. Bookings   
+
+Create table Movies (
+    movie_id int not null auto_increment,
+    movie_name varchar (60) not null,
+    movie_cast varchar(100),
+    movie_director varchar(60),
+    movie_img_fn varchar(20),
+    movie_language varchar(15),
+    movie_rel_date date,
+    primary key(movie_id));
+
+
+
+
 
 
 
---
+++ database/sqls.php
@@ -0,0 +1,19 @@
+//sample sqls 
+
+insert into Movies (movie_name, movie_cast, movie_director, movie_img_fn, 
+movie_language, movie_rel_date) values ('Dangal', 'Amir Khan', 'Nitesh Tiwari', 
+'1dangal.jpeg','Hindi','2016-12-23');
+
+insert into Movies (movie_name, movie_cast, movie_director, movie_img_fn, 
+movie_language, movie_rel_date) values ('Thuppakki','Vijay & Kajal', 'Murugadoss', 
+'2thuppaki.jpeg','Tamil','2012-11-13');
+
+insert into Movies (movie_name, movie_cast, movie_director, movie_img_fn, 
+movie_language, movie_rel_date) values ('Drishyam','Mohanlal & Meena', 'Jeetu Joseph', 
+'3drishyam.jpg','Malayalam','2013-12-19');
+
+insert into Movies (movie_name, movie_cast, movie_director, movie_img_fn, 
+movie_language, movie_rel_date) values ('Bahubali','Prabhas, Anushka & Rana', 
+'Raj Mouli', '4bahubali.jpg','Telegu', '2015-07-10');
+
+
